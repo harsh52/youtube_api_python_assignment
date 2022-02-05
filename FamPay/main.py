eb91c2b7fd478d7bd9cb2c6cf5f589ab6544dfda
harsh52 <hkumar8897@gmail.com>
2022-02-05 20:25:43 +0530

    Added docstring

--- FamPay/main.py
+++ FamPay/main.py
@@ -8,7 +8,9 @@ sched = BackgroundScheduler()
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
-
+    """
+    Entry point of the application, running async scheduled job and flaks app.
+    """
     sched.add_job(run, 'interval', seconds=schedule_time)
     sched.start()
 
