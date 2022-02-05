b7f200ebc905bb6ebb2e28ce08a9e60a162e3a53
harsh52 <hkumar8897@gmail.com>
2022-02-05 19:10:42 +0530

    Added config file contians basic config about application api like api key

---
+++ FamPay/config/api_config.py
@@ -0,0 +1,9 @@
+from apiclient.discovery import build
+
+SEARCH_KEYWORD = "programming"
+script_name = "yt_search_api"
+schedule_time = 10
+key_list = ["AIzaSyD8P2fpFur466vOVIUnyakxKtTj14qiauk", "AIzaSyD0oqziU-UUaSullhcjnMvvE205DHOh52c",
+            "AIzaSyC-clU8rF8avahqz6RD4t029peeWQUfRmo", "AIzaSyD5LxRFiLfW8C674EQ8hbgwG82-lqDrgbk"]
+
+# print(creat_resource())
