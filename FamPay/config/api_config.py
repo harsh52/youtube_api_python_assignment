0d024dba5860160bb70d584f9b6953ed83d770bc
harsh52 <hkumar8897@gmail.com>
2022-02-05 20:25:03 +0530

    Added docstring

--- FamPay/config/api_config.py
+++ FamPay/config/api_config.py
@@ -2,8 +2,8 @@ from apiclient.discovery import build
 
 SEARCH_KEYWORD = "programming"
 script_name = "yt_search_api"
-schedule_time = 10
+schedule_time = 120
+# Remove api in production, store it into Vault.
 key_list = ["AIzaSyD8P2fpFur466vOVIUnyakxKtTj14qiauk", "AIzaSyD0oqziU-UUaSullhcjnMvvE205DHOh52c",
             "AIzaSyC-clU8rF8avahqz6RD4t029peeWQUfRmo", "AIzaSyD5LxRFiLfW8C674EQ8hbgwG82-lqDrgbk"]
 
-# print(creat_resource())
